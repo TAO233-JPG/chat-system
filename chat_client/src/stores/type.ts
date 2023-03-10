6c3dd918a6b488b3029a8d036021655d661032e8
TAO233-JPG <1040581451@qq.com>
2023-03-10 22:18:56 +0800

    添加部分type

---
+++ chat_client/src/stores/type.ts
@@ -0,0 +1,20 @@
+export type UserT = {
+  username: string
+  uId: string
+  chatRooms: ChatRoomT[]
+}
+
+export type ChatRoomTypeT = 'Group' | 'Private'
+
+export type ChatRoomT = {
+  cId: string
+  name: string
+  type: ChatRoomTypeT
+}
+
+export type MessageT = {
+  uId: string
+  name: string
+  messageContent: string
+  sendingTime?: Date // TODO: 时间类型需要修改
+}
