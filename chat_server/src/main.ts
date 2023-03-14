dc42633151a35366674220889b998bc381668144
TAO233-JPG <1040581451@qq.com>
2023-03-14 12:52:20 +0800

    server/测试全局守卫

--- chat_server/src/main.ts
+++ chat_server/src/main.ts
@@ -4,12 +4,14 @@ import { AppModule } from './app.module';
 import { HttpFilter } from './common/filter';
 
 import { Response } from './common/reponse';
+import { UserGuard } from './users/users.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new Response());
   app.useGlobalFilters(new HttpFilter());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new UserGuard());
   await app.listen(3000);
 }
 bootstrap();
