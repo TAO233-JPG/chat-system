6c280a827e8cb7c42b76630c966d7da9b3695f20
TAO233-JPG <1040581451@qq.com>
2023-03-18 23:36:01 +0800

    client/展示聊天室名称

--- chat_client/src/components/ChatInterface/ChatInterface.vue
+++ chat_client/src/components/ChatInterface/ChatInterface.vue
@@ -2,7 +2,7 @@
   <div class="chat-interface">
     <header class="chat-interface-header">
       <span></span>
-      <span>聊天室</span>
+      <span>{{ title }}</span>
       <span class="management">
         <el-icon size="18"><Management /></el-icon>
       </span>
@@ -17,6 +17,13 @@
 import MessageInputBox from '../MessageInputBox/MessageInputBox.vue'
 import ChatInfoDisplayArea from '../ChatInfoDisplayArea/ChatInfoDisplayArea.vue'
 import { Management } from '@element-plus/icons-vue'
+import useUserStore from '@/stores/user/user'
+import { computed } from 'vue'
+const userStore = useUserStore()
+
+const title = computed(() => {
+  return userStore.currentChatRoom?.name ?? '请打开一个聊天室'
+})
 </script>
 <style lang="scss" scoped>
 .chat-interface {
