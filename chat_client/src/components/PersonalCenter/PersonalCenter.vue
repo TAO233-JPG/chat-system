ade9d128e7fbe425e964fa3f1a6132f7fb616438
TAO233-JPG <1040581451@qq.com>
2023-03-18 23:30:05 +0800

    client/展示用户名

--- chat_client/src/components/PersonalCenter/PersonalCenter.vue
+++ chat_client/src/components/PersonalCenter/PersonalCenter.vue
@@ -2,7 +2,7 @@
   <div class="person-center">
     <div class="user">
       <el-avatar :size="40" :src="circleUrl" />
-      <span class="name">乌拉啦</span>
+      <span class="name">{{ userStore.user?.name }}</span>
     </div>
     <ul class="person-options">
       <el-button type="danger" @click="handleLogout" :icon="SwitchButton" circle />
@@ -11,11 +11,13 @@
 </template>
 <script setup lang="ts">
 import Chat from '@/server/ws/chat'
+import useUserStore from '@/stores/user/user'
 import { SwitchButton } from '@element-plus/icons-vue'
 import { useRouter } from 'vue-router'
 const circleUrl = 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
 const chat = Chat.getInstance()
 const router = useRouter()
+const userStore = useUserStore()
 const handleLogout = () => {
   chat.close()
   router.replace('/')
@@ -37,6 +39,7 @@ const handleLogout = () => {
   .user {
     user-select: none;
     cursor: pointer;
+    text-align: center;
     .name {
       margin-top: 6px;
       font-size: 14px;
